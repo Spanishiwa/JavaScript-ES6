b15e244f2c7facd9d378a9f79c6cf93db9852325
Michael Yabut <spanishiwa@gmail.com>
2016-10-26 11:34:39 -0700

    classes syntactic sugar exercise

--- myEach.js
+++ myEach.js
@@ -5,6 +5,16 @@ Array.prototype.myEach = function(callback) {
   return this;
 };
 
-[1,2,3,4,5].myEach((num) => {
-  console.log(`square of ${num} is ${num * num}`);
-});
+// [1,2,3,4,5].myEach((num) => {
+//   console.log(`square of ${num} is ${num * num}`);
+// });
+
+Array.prototype.myMap = function(func) {
+  let resultArr = [];
+  this.myEach(k => resultArr.push(func(k)));
+  return resultArr;
+};
+let b = [1,2,3,4,5];
+let c = b.myMap(k => (k * 2));
+console.log(`new mapped array is ${c}`);
+console.log(`input array is ${b}`);
