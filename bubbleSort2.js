b67d655ae667cfcb13ad24a2df7c08d8f0ac8b99
Michael Yabut <spanishiwa@gmail.com>
2016-10-27 15:22:16 -0700

    iterative bubblesort in javascript

--- bubbleSort2.js
+++ bubbleSort2.js
@@ -1,22 +1,19 @@
-let b = [1,4,5,2,3,4,2];
-
+let a = [1,5,2,3,2,1];
 function bubbleSort(arr) {
   let sorted = false;
+
   while (sorted === false) {
     sorted = true;
 
     for(let i = 0; i < arr.length - 1; i += 1) {
       if (arr[i] <= arr[i+1])
         continue;
-      else {
-        [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
-        sorted = false;
-      }
-
+        else {
+          [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+          sorted = false;
+        }
     }
   }
-return arr;
+  return arr;
 }
-
-console.log(bubbleSort(b));
-console.log(b);
+console.log(bubbleSort(a));
