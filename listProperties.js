888e76103a03a36ede2b45fded46c99ef0ed72af
Michael Yabut <spanishiwa@gmail.com>
2016-11-07 15:36:13 -0800

    Accessing objects and their properties exercises

---
+++ listProperties.js
@@ -0,0 +1,51 @@
+var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 };
+
+var library = [
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games',
+       readingStatus: false
+   }];
+
+function listProperties(obj) {
+  return Object.getOwnPropertyNames(obj);
+}
+
+// console.log(listProperties(library[0]).join(', '));
+
+function deleteProperty(obj, prop) {
+  console.log(`before ${obj.name} properties: ${listProperties(obj)}`);
+  delete obj[prop];
+  console.log(`after ${obj.name} properties: ${listProperties(obj)}`);
+}
+
+// deleteProperty(student, "sclass");
+// console.log(student);
+
+function objLength(obj) {
+  return listProperties(obj).length;
+}
+
+// console.log(objLength(student));
+
+function readingStatus(lib) {
+  lib.forEach(function(obj) {
+    let book = `${obj.title} by ${obj.author}`;
+    console.log(`${obj.readingStatus ? "Already read" : "Yet to read"} ${book}`);
+  });
+}
+
+readingStatus(library);
