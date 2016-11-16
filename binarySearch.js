467787ba90ae8615899ff120915096168402272f
Michael Yabut <spanishiwa@gmail.com>
2016-11-15 21:46:28 -0800

    binary search in javascript

---
+++ binarySearch.js
@@ -0,0 +1,17 @@
+function bsearch(nums, target) {
+  if (nums.length === 0) return -1;
+  let mid = Math.floor(nums.length/2);
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid+1);
+
+  if (target === nums[mid]) return mid;
+  else if (target < nums[mid]) return bsearch(left, target);
+  else {
+    let result = bsearch(right, target);
+    return result === -1 ? -1 : result + mid + 1;
+  }
+}
+
+console.log(`bsearch([1, 2, 3], 3) = ${bsearch([1, 2, 3], 3)}`);
+console.log(`bsearch([1, 2, 3], 2.5) = ${bsearch([1, 2, 3], 2.5)}`);
+console.log(`bsearch([1, 2, 2.5, 3, 4, 5], 2.5) = ${bsearch([1, 2, 2.5, 3, 4, 5], 2.5)}`);
