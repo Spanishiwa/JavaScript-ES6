36d34024b63046314a7430aa3a22464091672559
Michael Yabut <spanishiwa@gmail.com>
2016-11-17 22:58:29 -0800

    battleship leetcode problem

--- battle.js
+++ battle.js
@@ -1,4 +1,15 @@
-DIRS = [up, down, left, right
-[0, 1], [0, -1], [-1, 0], [1, 0]
-
-connectedpositions
+function countBattleShips(board) {
+  let count = 0;
+  let ship = 'X';
+  for (let r = 0; r < board.length; r += 1) {
+    for (let c = 0; c < board[r].length; c += 1) {
+      let pos = board[r][c];
+      if (pos === ship) {
+        if ((r === 0 || board[r-1][c] !== ship) && (c === 0 || board[r][c-1] !== ship)) {
+          count += 1;
+        }
+      }
+    }
+  }
+  return count;
+}
