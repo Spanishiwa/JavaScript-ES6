f432362cba9748f78479722ae5be0ead1ff7d2ad
Michael Yabut <spanishiwa@gmail.com>
2016-11-17 23:16:11 -0800

    refactored leetcode battleship problem for readability

--- battle.js
+++ battle.js
@@ -3,13 +3,23 @@ function countBattleShips(board) {
   let ship = 'X';
   for (let r = 0; r < board.length; r += 1) {
     for (let c = 0; c < board[r].length; c += 1) {
-      let pos = board[r][c];
-      if (pos === ship) {
-        if ((r === 0 || board[r-1][c] !== ship) && (c === 0 || board[r][c-1] !== ship)) {
-          count += 1;
-        }
+      if (startOfShip(board, r, c)) {
+        count += 1;
       }
     }
   }
   return count;
+}
+
+function startOfShip(board, r, c) {
+  let ship = 'X';
+  let pos = board[r][c];
+
+  if (pos === ship) {
+    if (r === 0 || board[r-1][c] !== ship && c === 0 || board[r][c-1] !== ship) {
+      return true;
+    }
+  }
+
+  return false;
 }
