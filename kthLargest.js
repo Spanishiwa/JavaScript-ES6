3dcb365222aeb6c2ea28d669b31eec722e01d9a2
Michael Yabut <spanishiwa@gmail.com>
2016-11-26 21:28:30 -0800

    refactored kth largest number for speed

--- kthLargest.js
+++ kthLargest.js
@@ -6,8 +6,8 @@ function findKthLargest(nums, k) {
       uniq.push(num);
     }
   });
-
-  return uniq.sort()[k];
+  let len = uniq.length;
+  return uniq.sort()[len-k];
 }
 
 console.log(`find kth largest: ${findKthLargest([3,2,1,5,6,4], 2)}`);
