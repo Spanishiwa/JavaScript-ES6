f2706ae62a012323ccca5abcfe32cd49f5668e76
Michael Yabut <spanishiwa@gmail.com>
2016-11-26 21:26:09 -0800

    added logic for find Kth largest number

--- kthLargest.js
+++ kthLargest.js
@@ -1,5 +1,13 @@
 function findKthLargest(nums, k) {
+  let uniq = [];
 
+  nums.forEach(function(num) {
+    if (!uniq.includes(num)) {
+      uniq.push(num);
+    }
+  });
+
+  return uniq.sort()[k];
 }
 
-console.log(findKthLargest([3,2,1,5,6,4], 2));
+console.log(`find kth largest: ${findKthLargest([3,2,1,5,6,4], 2)}`);
