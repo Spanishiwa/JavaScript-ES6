5a7c8c8436fb92ad9f70eae276720bbdbdaee92a
Michael Yabut <spanishiwa@gmail.com>
2016-11-27 11:25:04 -0800

    refactored code to catch negative values

--- increasingTripletSubsequence.js
+++ increasingTripletSubsequence.js
@@ -1,9 +1,8 @@
 function increasingTriplet(nums) {
   for (let i = 0; i < nums.length - 2; i += 1) {
     let sequential = nums[i+2] > nums[i+1] && nums[i+1] > nums[i];
-    let positive = nums[i+2] >= 0 && nums[i+1] >= 0 && nums[i] >= 0;
 
-    if (sequential && positive) {
+    if (sequential) {
       return true;
     }
   }
@@ -13,3 +12,5 @@ function increasingTriplet(nums) {
 
 console.log(increasingTriplet([1, 2, 3, 4, 5]));
 console.log(increasingTriplet([5, 4, 3, 2, 1]));
+console.log(increasingTriplet([1,2,-10,-8,-7]));
+console.log(increasingTriplet([5,1,5,5,2,5,4]));
