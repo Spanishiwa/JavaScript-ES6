e5a6ae1252798837fca6ada424f61f5fcce230e0
Michael Yabut <spanishiwa@gmail.com>
2016-11-26 21:41:16 -0800

    addressed lexicographical sort issue in find kth largest num

--- kthLargest.js
+++ kthLargest.js
@@ -1,18 +1,7 @@
 function findKthLargest(nums, k) {
-  let uniq = [];
-
-  nums.forEach(function(num) {
-    if (!uniq.includes(num)) {
-      uniq.push(num);
-    }
-  });
-  let len = uniq.length;
-  if ((len - k) >= 0) {
-    return uniq.sort()[len-k];
-  }
-  else {
-    return uniq.sort()[len-1];
-  }
+  return nums.sort(function(p, c) {
+    return p-c;
+  })[nums.length-k];
 }
 
 console.log(`find kth largest: ${findKthLargest([3,2,1,5,6,4], 2)}`);
