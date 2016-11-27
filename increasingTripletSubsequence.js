257bf2ffa2634add367edd2ef60eae5b1d7a64ed
Michael Yabut <spanishiwa@gmail.com>
2016-11-27 11:30:34 -0800

    triple for-loop debugging increasing triplet subsequence

--- increasingTripletSubsequence.js
+++ increasingTripletSubsequence.js
@@ -1,9 +1,16 @@
 function increasingTriplet(nums) {
-  for (let i = 0; i < nums.length - 2; i += 1) {
-    let sequential = nums[i+2] > nums[i+1] && nums[i+1] > nums[i];
 
-    if (sequential) {
-      return true;
+  for (let i = 0; i < nums.length - 2; i += 1) {
+    for (let j = i+1; j < i; j += 1) {
+      for (let k = i+2; k < j; k += 1) {
+        console.log(`i, j, k ${i}, ${j}, ${k}`);
+        let sequential = nums[i] < nums[j] && nums[j] < nums[k];
+        let valid = nums[i] < nums[k];
+
+        if (sequential && valid) {
+          return true;
+        }
+      }
     }
   }
 
@@ -11,6 +18,6 @@ function increasingTriplet(nums) {
 }
 
 console.log(increasingTriplet([1, 2, 3, 4, 5]));
-console.log(increasingTriplet([5, 4, 3, 2, 1]));
-console.log(increasingTriplet([1,2,-10,-8,-7]));
-console.log(increasingTriplet([5,1,5,5,2,5,4]));
+// console.log(increasingTriplet([5, 4, 3, 2, 1]));
+// console.log(increasingTriplet([1,2,-10,-8,-7]));
+// console.log(increasingTriplet([5,1,5,5,2,5,4]));
