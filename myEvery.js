3606facc253816886e80301fd8cbbd5ee18da903
Michael Yabut <spanishiwa@gmail.com>
2016-11-28 22:52:05 -0800

    using filter to complete my Every logic

--- myEvery.js
+++ myEvery.js
@@ -1,7 +1,10 @@
 function myEvery(arr, predicate) {
-
+  return arr.filter(predicate).length === arr.length;
 }
 
 function isBigEnough(elem) {
   return (elem >= 10);
 }
+
+console.log(myEvery([11,12,13,14], isBigEnough));
+console.log(myEvery([11,12,13,14, 7, 11], isBigEnough));
