ac4f27d3e84f9f818e09182562a2e8347ef4485c
Michael Yabut <spanishiwa@gmail.com>
2016-11-29 16:23:29 -0800

    more TDD automated testing practice with reduce

--- fashionInventory2.js
+++ fashionInventory2.js
@@ -1,43 +1,49 @@
-let inventory = [
-  {
-    name: 'Brunello Cucinelli',
-    shoes: [
-      {name: 'tasselled black low-top lace-up', price: 1000},
-      {name: 'tasselled green low-top lace-up', price: 1100},
-      {name: 'plain beige suede moccasin', price: 950},
-      {name: 'plain olive suede moccasin', price: 1050}
-    ]
-  },
-  {
-    name: 'Gucci',
-    shoes: [
-      {name: 'red leather laced sneakers', price: 800},
-      {name: 'black leather laced sneakers', price: 900}
-    ]
-  }
-];
+function designerAveragePrice(designer) {
+  let sum = designer.shoes.reduce(function(total, shoe) {
+    total += shoe.price;
+    return total;
+  }, 0);
 
-let expected = {
-  'designers': [
-    {
-      'name': 'Brunello Cucinelli',
-      'averagePrice': 1025
-    },
-    {
-      'name': 'Gucci',
-      'averagePrice': 850
-    }
-  ]
-};
-
-function designerAveragePrice(currentInventory) {
-
+  return (sum / designer.shoes.length);
 }
 
 function testAggregateAveragePrice() {
+  let inventory = [
+    {
+      name: 'Brunello Cucinelli',
+      shoes: [
+        {name: 'tasselled black low-top lace-up', price: 1000},
+        {name: 'tasselled green low-top lace-up', price: 1100},
+        {name: 'plain beige suede moccasin', price: 950},
+        {name: 'plain olive suede moccasin', price: 1050}
+      ]
+    },
+    {
+      name: 'Gucci',
+      shoes: [
+        {name: 'red leather laced sneakers', price: 800},
+        {name: 'black leather laced sneakers', price: 900}
+      ]
+    }
+  ];
+
+  let expected = {
+    'designers': [
+      {
+        'name': 'Brunello Cucinelli',
+        'averagePrice': 1025
+      },
+      {
+        'name': 'Gucci',
+        'averagePrice': 850
+      }
+    ]
+  };
+
   let actual = JSON.stringify(designerAveragePrice(inventory));
   let testName = "it should aggregate average price per designer";
   let expect = JSON.stringify(expected);
+
   return assertEqual(actual, expect, testName);
 }
 
@@ -49,5 +55,14 @@ function assertEqual(actual, expected, testName) {
     console.log(`failed: ${testName}, actual is ${actual} expected is ${expected}`);
   }
 }
-
-testAggregateAveragePrice();
+let obj1 = {
+  name: 'Brunello Cucinelli',
+  shoes: [
+    {name: 'tasselled black low-top lace-up', price: 1000},
+    {name: 'tasselled green low-top lace-up', price: 1100},
+    {name: 'plain beige suede moccasin', price: 950},
+    {name: 'plain olive suede moccasin', price: 1050}
+  ]
+};
+// testAggregateAveragePrice();
+console.log(designerAveragePrice(obj1));
